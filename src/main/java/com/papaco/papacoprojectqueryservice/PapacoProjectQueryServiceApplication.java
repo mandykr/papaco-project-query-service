cb3fab0c869e1d676b0db1a245918aae157bc881
mandykr <hongseokii92@gmail.com>
2022-12-04 21:49:04 +0900

    Initial commit

---
+++ src/main/java/com/papaco/papacoprojectqueryservice/PapacoProjectQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.papaco.papacoprojectqueryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapacoProjectQueryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapacoProjectQueryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/papaco/papacoprojectqueryservice/PapacoProjectQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papaco.papacoprojectqueryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapacoProjectQueryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
